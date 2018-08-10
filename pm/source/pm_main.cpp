503a419f68e984af07670d0fac0af8c8a15d2d9c
Rei <asterma@yahoo.com>
2018-08-10 19:40:59 -0400

    oops, remove DRM from pm

--- pm/source/pm_main.cpp
+++ pm/source/pm_main.cpp
@@ -77,13 +77,6 @@ void __appInit(void) {
     if (R_FAILED(rc))  {
         fatalSimple(0xCAFE << 4 | 6);
     }
-    
-    /* Check for exosphere API compatibility. */
-    u64 exosphere_cfg;
-    if (R_FAILED(splGetConfig((SplConfigItem)65000, &exosphere_cfg))) {
-        fatalSimple(0xCAFE << 4 | 0xFF);
-        /* TODO: Does PM need to know about target firmware/master key revision? If so, extract from exosphere_cfg. */
-    }
 }
 
 void __appExit(void) {

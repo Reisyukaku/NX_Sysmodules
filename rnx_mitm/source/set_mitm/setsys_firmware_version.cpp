e81755f662bf499b659041735181e5efe6e62a70
lookye <45557556+lookye@users.noreply.github.com>
2019-05-08 12:45:47 +0200

    Fix display version string

--- rnx_mitm/source/set_mitm/setsys_firmware_version.cpp
+++ rnx_mitm/source/set_mitm/setsys_firmware_version.cpp
@@ -61,7 +61,7 @@ void VersionManager::Initialize() {
     {
         char display_version[sizeof(g_ams_fw_version.display_version)] = {0};
         
-        snprintf(display_version, sizeof(display_version), "%s ReiNX(%s.%s)", g_ams_fw_version.display_version, VERSION_MAJOR, VERSION_MINOR);
+        snprintf(display_version, sizeof(display_version), "%s ReiNX(%d.%d)", g_ams_fw_version.display_version, VERSION_MAJOR, VERSION_MINOR);
 
         memcpy(g_ams_fw_version.display_version, display_version, sizeof(g_ams_fw_version.display_version));
     }

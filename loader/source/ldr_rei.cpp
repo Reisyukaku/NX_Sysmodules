e4d597fd32423140cadf2f041c0cbd7b32d74768
designgears <bobby@designgears.com>
2019-05-27 13:36:37 -0600

    Fix for layeredfs stuffs (#25)
    * Fixed layeredfs

--- loader/source/ldr_rei.cpp
+++ loader/source/ldr_rei.cpp
@@ -4,7 +4,6 @@
 #include "ldr_rei.hpp"
 
 Result RNXService::GetReiNXVersion(Out<u32> maj, Out<u32> min) {
-    u32 ret = 0;
     *maj.GetPointer() = VERSION_MAJOR;
     *min.GetPointer() = VERSION_MINOR;
     return 0;

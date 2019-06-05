6bad24638d6efd81e6adc4b7db2b0cc707629e45
JamePeng <jame_peng@sina.com>
2019-06-06 00:06:35 +0800

    fs_shim:Libnx already implement the function fsFileOperateRange and fsStorageOperateRange

--- rnx_mitm/source/fs_mitm/fs_shim.h
+++ rnx_mitm/source/fs_mitm/fs_shim.h
@@ -19,12 +19,6 @@ Result fsOpenFileSystemWithPatchFwd(Service* s, FsFileSystem* out, u64 titleId, 
 Result fsOpenFileSystemWithIdFwd(Service* s, FsFileSystem* out, u64 titleId, FsFileSystemType fsType, const char* contentPath);
 Result fsOpenSaveDataFileSystemFwd(Service* s, FsFileSystem* out, u8 inval, FsSave *save);
 
-/* Missing FS File commands. */
-Result fsFileOperateRange(FsFile* f, u32 op_id, u64 off, u64 len, FsRangeInfo *out);
-
-/* Missing FS Storage commands. */
-Result fsStorageOperateRange(FsStorage* s, u32 op_id, u64 off, u64 len, FsRangeInfo *out);
-
 #ifdef __cplusplus
 }
 #endif
